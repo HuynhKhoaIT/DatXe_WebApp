077e90f2a9ddc75cbc7ac154afbbaa73f2ec6f69
HuynhKhoaIT <>
2024-04-17 15:25:49 +0700

    .

--- source/src/app/dashboard/profile/page.tsx
+++ source/src/app/dashboard/profile/page.tsx
@@ -1,25 +1,17 @@
+"use client";
 import React from "react";
 import { Space } from "@mantine/core";
 import InfoProfile from "./_component/Info";
 import UserProfile from "./_component/User";
-import { callApi } from "@/lib";
-import apiConfig from "@/constants/apiConfig";
-export default async function ProfilePage() {
-  const profile = await callApi(apiConfig.account.getAccount, {});
-  async function handleUpdate({ formData }: any) {
-    "use server";
-    const res = await callApi(apiConfig.account.update, {
-      data: {
-        ...formData,
-      },
-    });
-    return res;
-  }
+import { useAccountDetail } from "../hooks/profile/useProfile";
+
+export default function ProfilePage() {
+  const { data: profile } = useAccountDetail();
   return (
     <div className="user-profile-wrapper">
       <InfoProfile />
       <Space h="md" />
-      <UserProfile myAccount={profile} handleUpdate={handleUpdate} />
+      <UserProfile myAccount={profile?.data} />
     </div>
   );
 }
