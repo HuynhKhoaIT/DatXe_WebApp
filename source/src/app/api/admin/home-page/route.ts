cf980506da3cefc74ff6ef309c6618c2d0249122
vantaihvp <vantai.itc@gmail.com>
2024-04-26 09:22:33 +0700

    .

--- source/src/app/api/admin/home-page/route.ts
+++ source/src/app/api/admin/home-page/route.ts
@@ -1,8 +1,7 @@
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/route";
-import { getGarageIdByDLBDID } from "@/app/libs/prisma/garage";
-import { createProductsHome, getProductsHome } from "@/app/libs/prisma/homepage";
+import { createProductsHome, deleteProductsHome, getProductsHome } from "@/app/libs/prisma/homepage";
 
 export async function GET(request: NextRequest) {
     try {
@@ -23,6 +22,16 @@ export async function POST(request: Request) {
     try {
         const json = await request.json();
         const product = await createProductsHome(json);
+        return NextResponse.json(product);
+    } catch (error: any) {
+        return new NextResponse(error.message, { status: 500 });
+    }
+}
+
+export async function DELETE(request: Request) {
+    try {
+        const json = await request.json();
+        const product = await deleteProductsHome(json.id);
         return NextResponse.json(product);
     } catch (error: any) {
         return new NextResponse(error.message, { status: 500 });
--- source/src/app/libs/prisma/homepage.tsx
+++ source/src/app/libs/prisma/homepage.tsx
@@ -22,11 +22,31 @@ export async function getProductsHome(isProduct: number) {
 
 export async function createProductsHome(params:any) {
     try {
+        let isProduct = 1;
+        if(params.isProduct == 'false' || params.isProduct == false){
+            isProduct = 0;
+        }
+        let index = 10;
+        if(params.index){
+            index = Number(params.index);
+        }
         return await prisma.productsHome.create({
             data:{
                 productId: params.id,
-                isProduct: params.isProduct,
+                isProduct: isProduct,
                 index: params.index
+            }
+        })
+    } catch (error) {
+        return { error };
+    }
+}
+
+export async function deleteProductsHome(productId:string) {
+    try {
+        return await prisma.productsHome.delete({
+            where:{
+                productId
             }
         })
     } catch (error) {
