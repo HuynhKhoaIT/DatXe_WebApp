83989b84dbaec2a56553bfb6f704490f48a414a9
HuynhKhoaIT <>
2024-06-10 10:08:22 +0700

    .

--- source/src/app/admin/(admin)/booking-ttdk/[bookingId]/BookingTTDKForm.tsx
+++ source/src/app/admin/(admin)/booking-ttdk/[bookingId]/BookingTTDKForm.tsx
@@ -1,3 +1,147 @@
-export default function BookingForm({ dataDetail }: any) {
-  return <></>;
+"use client";
+import FooterSavePage from "@/app/admin/_component/FooterSavePage";
+import {
+  Box,
+  Card,
+  Grid,
+  LoadingOverlay,
+  TextInput,
+  Textarea,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useAddBookingTTDK } from "../../hooks/bookingTTDK/useAddBookingTTDK";
+import { useEffect } from "react";
+import DateField from "@/app/components/form/DateField";
+import dayjs from "dayjs";
+import { useSession } from "next-auth/react";
+import { TimeInput } from "@mantine/dates";
+
+export default function BookingForm({ dataDetail, isLoading, isEditing }: any) {
+  const { data } = useSession();
+  const { updateItem, isPendingUpdate } = useAddBookingTTDK({
+    token: data?.user?.token,
+  });
+
+  console.log(data);
+  const form = useForm({
+    initialValues: {},
+    validate: {},
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        form.setInitialValues(dataDetail);
+        form.setValues(dataDetail);
+        if (dataDetail?.dateSchedule)
+          form.setFieldValue(
+            "dateSchedule",
+            dayjs(dataDetail?.dateSchedule).toDate()
+          );
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    if (isEditing) {
+      fetchData();
+    }
+  }, [dataDetail]);
+  const handleSubmit = async (values: any) => {
+    if (isEditing) {
+      updateItem(values);
+    }
+  };
+  return (
+    <Box pos="relative">
+      <LoadingOverlay
+        zIndex={99}
+        visible={isLoading}
+        overlayProps={{ radius: "sm", blur: 2 }}
+      />
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <Card withBorder shadow="sm">
+          <Grid gutter={12}>
+            <Grid.Col span={6}>
+              <TextInput
+                size="lg"
+                radius={0}
+                {...form.getInputProps("code")}
+                label="Mã đơn hàng"
+                type="text"
+                placeholder="Mã đơn hàng"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <TextInput
+                size="lg"
+                radius={0}
+                {...form.getInputProps("licensePlates")}
+                label="Biển số xe"
+                type="text"
+                placeholder="Biển số xe"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <TextInput
+                size="lg"
+                radius={0}
+                {...form.getInputProps("fullname")}
+                label="Tên khách hàng"
+                type="text"
+                placeholder="Tên khách hàng"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <TextInput
+                size="lg"
+                radius={0}
+                {...form.getInputProps("phone")}
+                label="Số điện thoại"
+                type="text"
+                placeholder="Số điện thoại"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <DateField
+                {...form.getInputProps("dateSchedule")}
+                label="Ngày hẹn"
+                placeholder="Ngày hẹn"
+                clearable={true}
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <TimeInput
+                size="lg"
+                radioGroup="0"
+                {...form.getInputProps("time")}
+                label="Giờ hẹn"
+                placeholder="Giờ hẹn"
+              />
+              {/* <DateField
+                {...form.getInputProps("dateSchedule")}
+                label="Giờ hẹn"
+                placeholder="Giờ hẹn"
+                clearable={true}
+              /> */}
+            </Grid.Col>
+            <Grid.Col span={12}>
+              <Textarea
+                size="lg"
+                radius={0}
+                label="Ghi chú của khách hàng"
+                minRows={2}
+                autosize={true}
+                {...form.getInputProps("note")}
+                placeholder="Ghi chú của khách hàng"
+              />
+            </Grid.Col>
+          </Grid>
+        </Card>
+        <FooterSavePage
+          saveLoading={isPendingUpdate}
+          okText={isEditing ? "Cập nhật" : "Thêm"}
+        />
+      </form>
+    </Box>
+  );
 }
--- source/src/app/admin/(admin)/hooks/bookingTTDK/useAddBookingTTDK.ts
+++ source/src/app/admin/(admin)/hooks/bookingTTDK/useAddBookingTTDK.ts
@@ -13,166 +13,46 @@ import useFetch from "@/app/hooks/useFetch";
 import { toast } from "react-toastify";
 const queryClient = new QueryClient();
 
-const addOrder = async (values: any): Promise<any> => {
-  const response = await fetch(`/api/admin/orders`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(values),
-  });
-  if (!response.ok) {
-    throw new ResponseError("Failed to insert new order", response);
-  }
-  return await response.json();
-};
-const handleDbDLBD = async (values: any): Promise<any> => {
-  const response = await fetch(`/api/admin/orders/asyncDLBD`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(values),
-  });
-  if (!response.ok) {
-    throw new ResponseError("Failed to insert new order", response);
-  }
-  return await response.json();
-};
-
-const updateOrder = async (values: any): Promise<any> => {
-  const response = await fetch(`/api/admin/orders/${values?.id}`, {
+const updateBooking = async (values: any, token: any): Promise<any> => {
+  const response = await fetch(`/api/admin/booking-ttdk/${values?.id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(values),
   });
   if (!response.ok) {
-    throw new ResponseError("Failed to insert new order", response);
-  }
-  return await response.json();
-};
-
-const updateStepOrder = async (values: any): Promise<any> => {
-  const response = await fetch(`/api/admin/orders/step`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      id: values?.id,
-      step: values?.step,
-      cancelReason: values?.cancelReason,
-    }),
-  });
-
-  if (!response.ok) {
-    throw new ResponseError("Failed to update step order", response);
+    throw new ResponseError("Failed to update booking ttdk", response);
   }
   return await response.json();
 };
 
 interface UseOrders {
-  addItem: any;
   updateItem: any;
-  updateStep: any;
-  brandOptions: any;
-  dbDLBD: any;
-  isLoadingBrand: boolean;
-  isPendingAdd: boolean;
   isPendingUpdate: boolean;
-  isPendingUpdateStep: boolean;
-  isPendingDlbd: boolean;
-  isSuccessAdd: boolean;
 }
 
-export const useAddOrder = ({ isBack = true, ...prop }: any): UseOrders => {
+export const useAddBookingTTDK = ({ token }: any): UseOrders => {
   const router = useRouter();
   const queryClient = useQueryClient();
   const searchParams = useSearchParams();
-  const {
-    mutate: addItem,
-    isPending: isPendingAdd,
-    isSuccess: isSuccessAdd,
-  } = useMutation({
-    mutationFn: addOrder,
-    onSuccess: () => {
-      if (!isBack) {
-        router.refresh();
-      } else {
-        router.push("/admin/order-manager");
-      }
-      toast.success("Thêm đơn hàng thành công");
-
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY.orders, searchParams.toString(), 1],
-      });
-    },
-  });
-
-  const { mutate: dbDLBD, isPending: isPendingDlbd } = useMutation({
-    mutationFn: handleDbDLBD,
-    onSuccess: () => {
-      router.push("/admin/order-manager");
-      toast.success("Đơn hàng lên sàn thành công");
-
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY.orders, searchParams.toString(), 1],
-      });
-    },
-  });
 
   const { mutate: updateItem, isPending: isPendingUpdate } = useMutation({
-    mutationFn: updateOrder,
-    onSuccess: () => {
-      if (!isBack) {
-        router.refresh();
-        prop.onClose();
-      } else {
-        router.back();
-      }
-
-      toast.success("Cập nhật đơn hàng thành công");
-
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY.orders, searchParams.toString(), 1],
-      });
+    mutationFn: (values) => {
+      return updateBooking(values, token);
     },
-  });
-
-  const { mutate: updateStep, isPending: isPendingUpdateStep } = useMutation({
-    mutationFn: updateStepOrder,
     onSuccess: () => {
       router.back();
-      toast.success("Cập nhật trạng thái thành công");
-
+      toast.success("Cập nhật đơn hàng thành công");
       queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY.orders, searchParams.toString(), 1],
+        queryKey: [QUERY_KEY.bookingTTDK, searchParams.toString(), 1],
       });
     },
   });
 
-  const { data: brandOptions, isLoading: isLoadingBrand } = useFetch({
-    queryKey: ["brandOptions"],
-    queryFn: () => getOptionsBrands(),
-    options: {
-      refetchOnWindowFocus: false,
-      staleTime: Infinity,
-      refetchInterval: false,
-    },
-  });
   return {
-    addItem,
     updateItem,
-    updateStep,
-    brandOptions,
-    isLoadingBrand,
-    isPendingAdd,
     isPendingUpdate,
-    isPendingUpdateStep,
-    dbDLBD,
-    isPendingDlbd,
-    isSuccessAdd,
   };
 };
