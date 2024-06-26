2d908010d7b424184b7eb96cd3bf564d98d51c74
vantaihvp <vantai.itc@gmail.com>
2024-06-26 22:50:23 +0700

    Update route.ts

--- source/src/app/api/admin/garage/my-garage/route.ts
+++ source/src/app/api/admin/garage/my-garage/route.ts
@@ -1,14 +1,13 @@
-import { getGarageByDlbdId } from "@/app/libs/prisma/garage";
+import { showGarage } from "@/app/libs/prisma/garage";
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "@/lib/auth";
-import { checkAuthToken } from "@/utils/auth";
 
 export async function GET(request: NextRequest) {
   try {
-    const session = await getSession();
+    const session:any = await getSession();
     if (session?.user) {
-      let garageId = Number(session.user?.garageId);
-      const garages = await getGarageByDlbdId(garageId);
+      let garageId = session.user?.garageId;
+      const garages = await showGarage(garageId);
       return NextResponse.json(garages);
     }
     throw new Error("Chua dang nhap");
